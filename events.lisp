04c573c4560e2e8be3380d34d68cf2df8bd63309
Olaf Merkert <olaf@m-merkert.de>
2012-10-02 11:35:30 +0200

    macros to connect closures to gtk events

--- events.lisp
+++ events.lisp
@@ -1,1 +1,26 @@
 (in-package :gtk-utils)
+
+(defmacro define-signals (&rest signal-names)
+  `(progn
+     ,@(mapcar
+        (lambda (signal-name)
+          (multiple-value-bind (signal-symbol signal-string)
+              (let ((signal-string (and (symbolp signal-name)
+                                        (symbol-name signal-name))))
+                (cond
+                  ;; signal name and string explicitly given:
+                  ((listp signal-name) (values-list signal-name))
+                  ;; signal symbol starts with on-, strip that in 
+                  ((begins-with signal-string "on-" :test #'string-equal)
+                   (values signal-name (subseq signal-string 3)))
+                  ;; otherwise add ON- to the signal-name
+                  (t (values (symb 'on- signal-name) signal-string))))
+            `(defmacro ,signal-symbol (object &body body)
+               `(connect-signal ,object ,',(format nil "~(~A~)" signal-string)
+                                (ilambda (event) ,@body)))))
+        signal-names)))
+
+(define-signals on-clicked on-toggled on-changed on-destroy)
+
+(defun default-destroy (window)
+  (on-destroy window (leave-gtk-main)))
--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,11 @@
   (:shadowing-import-from :gtk :range)
   (:use :cl :ol
         :gtk :gdk :gobject)
-  (:export))
+  (:export
+   :define-signals
+   :on-clicked
+   :on-toggled
+   :on-changed
+   :on-destroy
+   :default-destroy
+   :event))
